0bd439f518766d2b115ff13021b54022f5457366
Aditya <aditya.kantipudi@protonmail.com>
2023-06-09 19:55:01 +0530

    Update Dog.js

--- Dog.js
+++ Dog.js
@@ -8,22 +8,24 @@ class Dog {
     getDogHtml() {
         console.log("getDogHtml is called")
         const { name, age, avatar, bio } = this;
-        return `    <header>
-        <img src="images/icon-profile.png" class="header_logo side_logo" alt="profile">
-        <img src="images/logo.png" class="header_logo center_logo" alt="logo">
-        <img src="images/icon-chat.png" class="header_logo side_logo" alt="chat icon">
-    </header>
-    <main>
-        <section class="sec1">
-            <img src=${avatar} class="dog_img" alt="dog rex image">
-            <div class="name">${name},${age}</div>
-            <div class="bio">${bio}</div>
-        </section>
-    </main>
-    <footer>
-        <img src="images/icon-cross.png" id="cross" alt="cross icon">
-        <img src="images/icon-heart.png" id="heart" alt="heart icon">
-    </footer>`
+        return `    
+        
+                    <header>
+                        <img src="images/icon-profile.png" class="header_logo side_logo" alt="profile">
+                        <img src="images/logo.png" class="header_logo center_logo" alt="logo">
+                        <img src="images/icon-chat.png" class="header_logo side_logo" alt="chat icon">
+                    </header>
+                    <main>
+                        <section class="sec1">
+                            <img src=${avatar} class="dog_img" alt="dog rex image">
+                            <div class="name">${name},${age}</div>
+                            <div class="bio">${bio}</div>
+                        </section>
+                    </main>
+                    <footer>
+                        <img src="images/icon-cross.png" id="cross" alt="cross icon">
+                        <img src="images/icon-heart.png" id="heart" alt="heart icon">
+                    </footer>`
     }
 
 }
