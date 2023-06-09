96d1b84fe393139aad643815787bd10ed12a6cdf
Aditya <aditya.kantipudi@protonmail.com>
2023-06-09 19:53:19 +0530

    ..

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import dogData from "./data";
 import Dog from "./Dog";
 
+let dog1 = new Dog(dogData[0])
+render()
 
 function render() {
     console.log('render function is called')
     document.getElementById('main').innerHTML = dog1.getDogHtml()
 }
 
-let dog1 = new Dog(dogData[0])
-render()
+
+
 console.log('hello world')
